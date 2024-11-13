d13460ca1f260ea8b01b5dbf43ae40c633e17ed2
vivek pandey <udyalakvivek@gmail.com>
2024-11-13 22:23:06 +0530

    formspree update

--- script.js
+++ script.js
@@ -7,17 +7,11 @@ function hideslidebar() {
     const sidebar = document.querySelector(".sidebar")
     sidebar.style.display = 'none'
 }
-// send message 
 
-// let button = document.querySelector('#button');
-// button.addEventListener('click', () => {
-//     alert(`Your Message has been sent`);
-
-// });
 
 
 function emptyMsg(event) {
-    event.preventDefault()
+    // event.preventDefault()
     const name = document.querySelector('input[type="text"]').value;
     const email = document.querySelector('input[type="email"]').value;
     const message = document.getElementById('input3').value;
@@ -28,40 +22,17 @@ function emptyMsg(event) {
     else {
 
         alert(`Your Message has been sent`);
-        document.querySelector('.form').reset();
-        // name.value=""
-        // email.value=""
-        // message.value=""
+        // document.querySelector('.form').reset();
+        setTimeout(() => {
+            document.querySelector('.form').reset();
+            console.log("Form is reset ")
+        }, 1000);
     }
 
 }
 
 
-// Dark mode 
-// let modebtn = document.querySelector('#moonli')
-// let home= document.querySelector('#home')
 
-// let currentMode = "light"
-// modebtn.addEventListener("click", () => {
-//     console.log("btn is clicked");
-//     console.log(currentMode);
-
-//     if (currentMode == "dark") {
-//         currentMode = "light"
-//         document.querySelector('body').style.backgroundColor = "#fff"
-//         document.querySelector('body').style.color = "#212121"
-//         home.style.backgroundColor="#fff"
-//         home.style.color="#212121"
-
-
-//     } else {
-//         currentMode = "dark"
-//         document.querySelector('body').style.backgroundColor = "#212121"
-//         document.querySelector('body').style.color = "#fff"
-//     }
-// }
-
-// )
 
 
 
